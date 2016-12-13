2d264af50880ff1f777de175a93c70cd9f3bf53e
arekzaluski <arkadiusz.zaluski@arm.com>
2016-12-13 13:49:43 +0000

    Add main page to docs

---
+++ docs.md
@@ -0,0 +1,12 @@
+# .Net mbed Cloud SDK
+
+The mbed Cloud SDK gives developers access to the full mbed suite using .Net 4.5.
+
+Other languages are available too:
+
+- [Python](https://github.com/ARMmbed/mbed-cloud-sdk-python)
+- [Javascript](https://github.com/ARMmbed/mbed-cloud-sdk-javascript)
+
+If you want to contribute to creating a SDK for another language the work is
+greatly appreciated and you can read more about the process
+[here](https://github.com/ARMmbed/mbed-cloud-sdk-codegen)
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['listapikeys',['ListApiKeys',['../classmbed_cloud_s_d_k_1_1_access_1_1_access.html#a4d723aff2e484ad55d954c5d99c33d9e',1,'mbedCloudSDK::Access::Access']]],
-  ['listdevices',['ListDevices',['../classmbed_cloud_s_d_k_1_1_devices_1_1_devices.html#a9776d53b005792e63b0df93bfb624fcc',1,'mbedCloudSDK::Devices::Devices']]],
+  ['listdevices',['ListDevices',['../classmbed_cloud_s_d_k_1_1_devices_1_1_devices.html#a096f50e6c189d6ccd73a5a52e5c631ef',1,'mbedCloudSDK::Devices::Devices']]],
   ['listendpoints',['ListEndpoints',['../classmbed_cloud_s_d_k_1_1_devices_1_1_devices.html#add4313896ed6964943e83c2543b2f025',1,'mbedCloudSDK::Devices::Devices']]],
   ['listgroups',['ListGroups',['../classmbed_cloud_s_d_k_1_1_access_1_1_access.html#adf24c473a49577314757be44aa6ad2b6',1,'mbedCloudSDK::Access::Access']]],
   ['listparams',['ListParams',['../classmbed_cloud_s_d_k_1_1_common_1_1_list_params.html#af395b456439dd525373d1698d4ac839b',1,'mbedCloudSDK::Common::ListParams']]],
