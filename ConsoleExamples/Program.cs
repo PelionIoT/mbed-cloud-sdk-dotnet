f6c14caf02e2e7867cf3d04f8981799ab1be19bc
arekzaluski <arkadiusz.zaluski@arm.com>
2016-12-16 11:30:41 +0000

    Fix indentation

--- ConsoleExamples/Program.cs
+++ ConsoleExamples/Program.cs
@@ -40,56 +40,54 @@ namespace ConsoleExamples
         private static void runEndpointsExample(Config config)
         {
             Devices devices = new Devices(config);
-			foreach (var endpoint in devices.ListEndpoints())
+            foreach (var endpoint in devices.ListEndpoints())
             {
                 Console.WriteLine(endpoint);
             }
         }
 
-		private static void runDevicesExample(Config config)
-		{
-			Devices devices = new Devices(config);
-			foreach (var device in devices.ListDevices())
-			{
-				Console.WriteLine(device.ToString());
-			}
-		}
+        private static void runDevicesExample(Config config){
+            Devices devices = new Devices(config);
+            foreach (var device in devices.ListDevices())
+            {
+                Console.WriteLine(device.ToString());
+            }
+        }
 
-		private static void runSubscriptionExample(Config config)
-		{
-			var buttonResource = "/3200/0/5501";
-			Devices devices = new Devices(config);
-			var endpoints = devices.ListEndpoints();
-			if (endpoints == null)
-			{
-				throw new Exception("No endpoints registered. Aborting.");
-			}
-			devices.StartLongPolling();
-			AsyncConsumer<String> consumer =  devices.Subscribe(endpoints[0].Name, buttonResource);
-			while (true)
-			{
-				Task<string> t = consumer.GetValue();
-				Console.WriteLine(t.Result);
-			}
-		}
+        private static void runSubscriptionExample(Config config)
+        {
+            var buttonResource = "/3200/0/5501";
+            Devices devices = new Devices(config);
+            var endpoints = devices.ListEndpoints();
+            if (endpoints == null)
+            {
+                throw new Exception("No endpoints registered. Aborting.");
+            }
+            devices.StartLongPolling();
+            AsyncConsumer<String> consumer =  devices.Subscribe(endpoints[0].Name, buttonResource);
+            while (true)
+            {
+                Task<string> t = consumer.GetValue();
+                Console.WriteLine(t.Result);
+            }
+        }
 
-		private static void runWebhookExample(Config config)
-		{
-			var buttonResource = "/3200/0/5501";
-			Devices devices = new Devices(config);
-			var endpoints = devices.ListEndpoints();
-			if (endpoints == null)
-			{
-				throw new Exception("No endpoints registered. Aborting.");
-			}
-			string webhook = "http://testwebhooks.requestcatcher.com/test";
-			devices.RegisterWebhook(webhook);
-			Thread.Sleep(2000);
-			devices.Subscribe(endpoints[0].Name, buttonResource);
-			Console.WriteLine(string.Format("Webhook registered, see output on {0}", webhook));
-			Thread.Sleep(20000);
-			devices.DeregisterWebhooks();
-		}
-
+        private static void runWebhookExample(Config config)
+        {
+            var buttonResource = "/3200/0/5501";
+            Devices devices = new Devices(config);
+            var endpoints = devices.ListEndpoints();
+            if (endpoints == null)
+            {
+                throw new Exception("No endpoints registered. Aborting.");
+            }
+            string webhook = "http://testwebhooks.requestcatcher.com/test";
+            devices.RegisterWebhook(webhook);
+            Thread.Sleep(2000);
+            devices.Subscribe(endpoints[0].Name, buttonResource);
+            Console.WriteLine(string.Format("Webhook registered, see output on {0}", webhook));
+            Thread.Sleep(20000);
+            devices.DeregisterWebhooks();
+        }
     }
 }
