a2da1147fa9aac3ffaa05079cff1e549d0567f08
Arkadiusz Zaluski <Arkadiusz.Zaluski@arm.com>
2017-02-01 16:20:31 +0000

    Add example for dev certificate creation

--- ConsoleExamples/Examples/Development/CreateDeveloperCertificate.cs
+++ ConsoleExamples/Examples/Development/CreateDeveloperCertificate.cs
@@ -1,4 +1,5 @@
-﻿using mbedCloudSDK.Common;
+﻿using mbedCloudSDK.Access.Api;
+using mbedCloudSDK.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,18 +19,50 @@ namespace ConsoleExamples.Examples.Development
             this.config = config;
         }
 
+        private string certificate = @"
+            //----------------------------------------------------------------------------
+            //   The confidential and proprietary information contained in this file may
+            //   only be used by a person authorised under and to the extent permitted
+            //   by a subsisting licensing agreement from ARM Limited or its affiliates.
+            //
+            //          (C) COPYRIGHT 2013-2017 ARM Limited or its affiliates.
+            //              ALL RIGHTS RESERVED
+            //
+            //   This entire notice must be reproduced on all copies of this file
+            //   and copies of this file may only be made by a person if such person is
+            //   permitted to do so under the terms of a subsisting license agreement
+            //   from ARM Limited or its affiliates.
+            //----------------------------------------------------------------------------
+            /*
+            {0}
+            {1}*/
+            # ifndef __IDENTITY_DEV_SECURITY_H__
+            #define __IDENTITY_DEV_SECURITY_H__
+            # include <inttypes.h>
+                    const char gIdcDevSecurityAccountId[33] = '{2}';
+                    const uint8_t gIdcDevSecurityPrivateSignKey[32] = {{
+            {3}
+            }};
+            #endif //__IDENTITY_DEV_SECURITY_H";
+
         public void CreateCertificate()
         {
             CngKey key = CngKey.Create(CngAlgorithm.ECDsaP256, null, new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextExport });
-            ECDsaCng dsa = new ECDsaCng(key);
-            byte[] privateBytes = key.Export(CngKeyBlobFormat.EccPrivateBlob);
-            string privateString = Encoding.UTF8.GetString(privateBytes);
+            byte[] keyBytes = key.Export(CngKeyBlobFormat.EccPrivateBlob);
             byte[] publicBytes = key.Export(CngKeyBlobFormat.EccPublicBlob);
-            byte[] pemPrivate = key.Export(CngKeyBlobFormat.Pkcs8PrivateBlob);
-            string hex = BitConverter.ToString(pemPrivate);
-            hex = hex.Replace("-", "");
-            //string pemString = Encoding.UTF8.GetString(pemPrivate);
-            Console.WriteLine(privateBytes);
+            byte[] privateKeyBytes = new byte[32];
+            // Private key is on the last 32 bytes of key.
+            Array.Copy(keyBytes, 72, privateKeyBytes, 0, 32);
+            string privateHex = string.Join(",", privateKeyBytes.Select(b =>
+            {
+                return String.Format("0x{0}", b.ToString());
+            }));
+            var accessApi = new AccessApi(config);
+            var accountId = accessApi.GetAccount().Id;
+            string privatePem = Convert.ToBase64String(privateKeyBytes); 
+            string publicPem = Convert.ToBase64String(publicBytes);
+            String certString = String.Format(certificate, publicPem, privatePem, accountId, privateHex);
+            Console.WriteLine(certString);
         }
     }
 }
