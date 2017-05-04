a7021b5a159cde899b06934c5b2c44b9aa093371
mbed Cloud (Circle CI) <support@mbed.com>
2017-05-04 09:54:29 +0000

    Add generated changes (2017-05-04 09:54:29.031823)

--- mbedCloudSDK/Backends/DeviceCatalog/Model/DeviceDataPostRequest.cs
+++ mbedCloudSDK/Backends/DeviceCatalog/Model/DeviceDataPostRequest.cs
@@ -1,7 +1,7 @@
 /* 
- * Device Catalog API
+ * Device Directory API
  *
- * This is the API Documentation for the mbed device catalog update service.
+ * This is the API Documentation for the mbed device directory update service.
  *
  * OpenAPI spec version: 0.1
  * 
