1d3b7c983084127be008b59f2d3866c6325c4b9f
arekzaluski <akadiusz.zaluski@arm.com>
2017-05-04 03:34:46 +0100

    Fixes after merging with master.

---
+++ mbedCloudSDK/Statistics/Model/StatisticsData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbedCloudSDK.Statistics.Model
+{
+    class StatisticsData
+    {
+        /// <summary>
+        /// Number of successful bootstrap certificate create requests account has used.
+        /// </summary>
+        /// <value>Number of successful bootstrap certificate create requests account has used.</value>
+        public long? BootstrapCertificateCreate { get; set; }
+        
+        /// <summary>
+        /// Number of suuccessful connector rest API requests account has used.
+        /// </summary>
+        /// <value>Number of suuccessful connector rest API requests account has used.</value>
+        public long? ConnectorCaRestApiCount { get; set; }
+        
+        /// <summary>
+        /// Number of failed bootstraps account has used.
+        /// </summary>
+        /// <value>Number of failed bootstraps account has used.</value>
+        public long? BootstrapsFailed { get; set; }
+        
+        /// <summary>
+        /// Number of failed connector rest API requests account has used.
+        /// </summary>
+        /// <value>Number of failed connector rest API requests account has used.</value>
+        public long? ConnectorCaRestApiErrorCount { get; set; }
+        
+        /// <summary>
+        /// Number of successful connector full credentials get requests account has used.
+        /// </summary>
+        /// <value>Number of successful connector full credentials get requests account has used.</value>
+        public long? ConnectorFullCredentialsGet { get; set; }
+        
+        /// <summary>
+        /// Number of successful bootstrap certificate delete requests account has used.
+        /// </summary>
+        /// <value>Number of successful bootstrap certificate delete requests account has used.</value>
+        public long? BootstrapCertificateDelete { get; set; }
+        
+        /// <summary>
+        /// UTC time in RFC3339 format
+        /// </summary>
+        /// <value>UTC time in RFC3339 format</value>
+        public string Timestamp { get; set; }
+        
+        /// <summary>
+        /// Number of pending bootstraps account has used.
+        /// </summary>
+        /// <value>Number of pending bootstraps account has used.</value>
+        public long? BootstrapsPending { get; set; }
+        
+        /// <summary>
+        /// Number of successful connector certificate create requests account has used.
+        /// </summary>
+        /// <value>Number of successful connector certificate create requests account has used.</value>
+        public long? ConnectorCertificateCreate { get; set; }
+        
+        /// <summary>
+        /// Number of successful bootstrap full credentials get requests account has used.
+        /// </summary>
+        /// <value>Number of successful bootstrap full credentials get requests account has used.</value>
+        public long? BootstrapFullCredentialsGet { get; set; }
+        
+        /// <summary>
+        /// Number of successful connector certificate create requests account has used.
+        /// </summary>
+        /// <value>Number of successful connector certificate create requests account has used.</value>
+        public long? ConnectorCertificateDelete { get; set; }
+        
+        /// <summary>
+        /// Number of successful connector credentials get requests account has used.
+        /// </summary>
+        /// <value>Number of successful connector credentials get requests account has used.</value>
+        public long? ConnectorCredentialsGet { get; set; }
+        
+        /// <summary>
+        /// Number of successful bootstrap credentials get requests account has used.
+        /// </summary>
+        /// <value>Number of successful bootstrap credentials get requests account has used.</value>
+        public long? BootstrapCredentialsGet { get; set; }
+        
+        /// <summary>
+        /// Number of successful bootstraps account has used.
+        /// </summary>
+        /// <value>Number of successful bootstraps account has used.</value>
+        public long? BootstrapsSuccessful { get; set; }
+
+        public static StatisticsData Map(statistics.Model.Metric statisticsData)
+        {
+            StatisticsData statistic = new StatisticsData();
+            statistic.BootstrapsFailed = statisticsData.BootstrapsFailed;
+            statistic.Timestamp = statisticsData.Timestamp;
+            statistic.BootstrapsPending = statisticsData.BootstrapsPending;
+            statistic.BootstrapsSuccessful = statisticsData.BootstrapsSuccessful;
+            return statistic;
+        }
+        
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class StatisticsData {\n");
+            sb.Append("  BootstrapCertificateCreate: ").Append(BootstrapCertificateCreate).Append("\n");
+            sb.Append("  ConnectorCaRestApiCount: ").Append(ConnectorCaRestApiCount).Append("\n");
+            sb.Append("  BootstrapsFailed: ").Append(BootstrapsFailed).Append("\n");
+            sb.Append("  ConnectorCaRestApiErrorCount: ").Append(ConnectorCaRestApiErrorCount).Append("\n");
+            sb.Append("  ConnectorFullCredentialsGet: ").Append(ConnectorFullCredentialsGet).Append("\n");
+            sb.Append("  BootstrapCertificateDelete: ").Append(BootstrapCertificateDelete).Append("\n");
+            sb.Append("  Timestamp: ").Append(Timestamp).Append("\n");
+            sb.Append("  BootstrapsPending: ").Append(BootstrapsPending).Append("\n");
+            sb.Append("  ConnectorCertificateCreate: ").Append(ConnectorCertificateCreate).Append("\n");
+            sb.Append("  BootstrapFullCredentialsGet: ").Append(BootstrapFullCredentialsGet).Append("\n");
+            sb.Append("  ConnectorCertificateDelete: ").Append(ConnectorCertificateDelete).Append("\n");
+            sb.Append("  ConnectorCredentialsGet: ").Append(ConnectorCredentialsGet).Append("\n");
+            sb.Append("  BootstrapCredentialsGet: ").Append(BootstrapCredentialsGet).Append("\n");
+            sb.Append("  BootstrapsSuccessful: ").Append(BootstrapsSuccessful).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ mbedCloudSDK/Statistics/Model/StatisticsQueryOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbedCloudSDK.Statistics.Model
+{
+    class StatisticsQueryOptions
+    {
+        /// <summary>
+        /// Comma-separated list of requested metrics.Supported values are bootstraps_successful, bootstraps_failed, bootstraps_pending, bootstrap_certificate_create, bootstrap_certificate_delete, connector_certificate_create, connector_certificate_delete, bootstrap_credentials_get, bootstrap_full_credentials_get, connector_credentials_get, connector_full_credentials_get, connector_ca_rest_api_count, connector_ca_rest_api_error_count
+        /// </summary>
+        public string Include { get; set; }
+
+        /// <summary>
+        /// UTC time/year/date in RFC3339 format. Fetch data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The parameter is not mandatory, if period specified.
+        /// </summary>
+        public string Start { get; set; }
+
+        /// <summary>
+        /// UTC time / year / date in RFC3339 format. Fetch data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207.The parameter is not mandatory, if period specified. 
+        /// </summary>
+        public string End { get; set; }
+
+        /// <summary>
+        /// Period. Fetch data for the period in days, weeks or hours. Sample values: 2h, 3w, 4d. The parameter is not mandatory, if start and end time are specified. 
+        /// </summary>
+        public string Period { get; set; }
+
+        /// <summary>
+        /// Group data by this interval in days, weeks or hours. Sample values: 2h, 3w, 4d. 
+        /// </summary>
+        public string Interval { get; set; }
+
+        public StatisticsQueryOptions()
+        {
+            this.Include = "bootstraps_successful, bootstraps_failed, bootstraps_pending, bootstrap_certificate_create, bootstrap_certificate_delete, connector_certificate_create,  connector_certificate_delete, bootstrap_credentials_get, bootstrap_full_credentials_get, connector_credentials_get, connector_full_credentials_get, connector_ca_rest_api_count, connector_ca_rest_api_error_count";
+            this.Interval = "1d";
+            this.Period = "1m";
+        }
+    }
+}
