e19b1b3f10aa8ab9bce76c4ddc23be0edaa8c73a
alexl0gan <alex.logan@arm.com>
2017-08-21 15:39:47 +0100

    remove erroneous files

--- monotest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo certmgr -ssl -m https://go.microsoft.com
-sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
-sudo certmgr -ssl -m https://nuget.org
--- xbuildTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xbuild
-
-exit 0
