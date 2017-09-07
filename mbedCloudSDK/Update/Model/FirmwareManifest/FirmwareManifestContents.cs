be2bc51ac24fd90c1b7b9cfaba6f824447d983cb
alexl0gan <alex.logan@arm.com>
2017-09-07 20:28:37 +0100

    fix manifest model

--- mbedCloudSDK/Update/Model/FirmwareManifest/FirmwareManifestContents.cs
+++ mbedCloudSDK/Update/Model/FirmwareManifest/FirmwareManifestContents.cs
@@ -54,9 +54,24 @@ namespace mbedCloudSDK.Update.Model.FirmwareManifest
         /// <value>The device&#39;s 128-bit RFC4122 GUID as a hexidecimal digit string. Each device has a single, unique device ID.</value>
         public string DeviceId { get; set; }
         /// <summary>
-        /// Gets or Sets Payload
+        /// Gets or Sets Format
         /// </summary>
-        public ManifestContentsPayload Payload { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ManifestContentsPayloadFormatEnum PayloadFormat { get; set; }
+        /// <summary>
+        /// Gets or Sets Reference
+        /// </summary>
+        public ManifestContentsPayloadReference Reference { get; set; }
+        /// <summary>
+        /// An payload storage destination identifier. The identifier specifies where to place the firmware image on the device. For example, when an IoT device has multiple microcontrollers (MCUs), the identifier determines which MCU receives the image.
+        /// </summary>
+        /// <value>An payload storage destination identifier. The identifier specifies where to place the firmware image on the device. For example, when an IoT device has multiple microcontrollers (MCUs), the identifier determines which MCU receives the image.</value>
+        public string StorageIdentifier { get; set; }
+        /// <summary>
+        /// Size of the payload in bytes
+        /// </summary>
+        /// <value>Size of the payload in bytes</value>
+        public int? PayloadSize { get; set; }
 
         public static FirmwareManifestContents Map(update_service.Model.ManifestContents item)
         {
@@ -67,10 +82,14 @@ namespace mbedCloudSDK.Update.Model.FirmwareManifest
             contents.Description = item.Description;
             contents.Nonce = item.Nonce;
             contents.Timestamp = item.Timestamp;
-            contents.EncryptionMode = (ManifestContentsEncryptionModeEnum)Enum.Parse(typeof(ManifestContentsEncryptionModeEnum), item.EncryptionMode._Enum.Value.ToString());
+            contents.EncryptionMode = (ManifestContentsEncryptionModeEnum)item.EncryptionMode._Enum.Value;
             contents.ApplyImmediately = item.ApplyImmediately;
             contents.DeviceId = item.DeviceId;
-            contents.Payload = ManifestContentsPayload.Map(item.Payload);
+            var manifestContentsPayload = ManifestContentsPayload.Map(item.Payload);
+            contents.PayloadFormat = manifestContentsPayload.PayloadFormat;
+            contents.Reference = manifestContentsPayload.Reference;
+            contents.StorageIdentifier = manifestContentsPayload.StorageIdentifier;
+            contents.PayloadSize = contents.Reference.PayloadSize;
             return contents;
         }
     }
@@ -78,23 +97,23 @@ namespace mbedCloudSDK.Update.Model.FirmwareManifest
     public enum ManifestContentsEncryptionModeEnum 
     {
         [EnumMember(Value="none-ecc-secp256r1-sha256")]
-        noneEccSecp256r1Sha256,
+        noneEccSecp256r1Sha256 = 1,
         [EnumMember(Value = "aes-128-ctr-ecc-secp256r1-sha256")]
-        aes128CtrEccSecp256r1Sha256,
+        aes128CtrEccSecp256r1Sha256 = 2,
         [EnumMember(Value = "none-none-sha256")]
-        noneNoneSha256
+        noneNoneSha256 = 3
     }
 
     public enum ManifestContentsPayloadFormatEnum
     {
         [EnumMember(Value = "raw-binary")]
-        rawBinary,
+        rawBinary = 1,
         [EnumMember(Value = "cbor")]
-        cbor,
+        cbor = 2,
         [EnumMember(Value = "hex-location-length-data")]
-        hexLocationLengthData,
+        hexLocationLengthData = 3,
         [EnumMember(Value = "elf")]
-        elf
+        elf = 4
     }
 
     public class ManifestContentsPayloadReference
@@ -145,7 +164,7 @@ namespace mbedCloudSDK.Update.Model.FirmwareManifest
         public static ManifestContentsPayload Map(update_service.Model.ManifestContentsPayload item)
         {
             var payload = new ManifestContentsPayload();
-            payload.PayloadFormat = (ManifestContentsPayloadFormatEnum)Enum.Parse(typeof(ManifestContentsPayloadFormatEnum), item.Format._Enum.Value.ToString());
+            payload.PayloadFormat = (ManifestContentsPayloadFormatEnum)item.Format._Enum.Value;
             payload.Reference = ManifestContentsPayloadReference.Map(item.Reference);
             payload.StorageIdentifier = item.StorageIdentifier;
             return payload;
