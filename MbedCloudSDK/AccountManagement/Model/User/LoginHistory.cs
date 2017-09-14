3961a2550c6abf02699d05aa29d0c010aa6daab5
Alex Logan <30593755+alexl0gan@users.noreply.github.com>
2017-09-14 10:55:21 +0100

    Add login history and 2FA flag (#68)
    * add login history and 2FA flag
    
    * permissions on login history

---
+++ MbedCloudSDK/AccountManagement/Model/User/LoginHistory.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace MbedCloudSDK.AccountManagement.Model.User
+{
+    ///<summary>
+    /// Login History
+    ///</summary>
+    public class LoginHistory
+    {
+        ///<summary>
+        /// Date of login
+        ///</summary>
+        public DateTime? Date { get; private set; }
+
+        ///<summary>
+        /// User agent used for login
+        ///</summary>
+        public string UserAgent { get; private set; }
+
+        ///<summary>
+        /// IP Address login from
+        ///</summary>
+        public string IpAddress { get; private set; }
+
+        ///<summary>
+        /// Whether login was successful
+        ///</summary>
+        public bool? Success { get; private set; }
+
+        public LoginHistory(DateTime? date = null, string userAgent = null, string ipAddress = null, bool? success = null)
+        {
+            Date = date;
+            UserAgent = userAgent;
+            IpAddress = ipAddress;
+            Success = success;
+        }
+
+        public static LoginHistory Map(iam.Model.LoginHistory data)
+        {
+            return new LoginHistory(data.Date, data.UserAgent, data.IpAddress, data.Success);
+        }
+
+        public static List<LoginHistory> MapList(List<iam.Model.LoginHistory> data)
+        {
+            var list = new List<LoginHistory>();
+            foreach (var item in data)
+            {
+                list.Add(LoginHistory.Map(item));
+            }
+            return list;
+        } 
+    }
+}
--- MbedCloudSDK/AccountManagement/Model/User/User.cs
+++ MbedCloudSDK/AccountManagement/Model/User/User.cs
@@ -23,22 +23,22 @@ namespace MbedCloudSDK.AccountManagement.Model.User
         /// A username containing alphanumerical letters and -,._@+&#x3D; characters.
         /// </summary>
         public string Username { get; set; }
-        
+
         /// <summary>
         /// A flag indicating whether the user&#39;s email address has been verified or not.
         /// </summary>
         public bool? EmailVerified { get; private set; }
-        
+
         /// <summary>
         /// The UUID of the account.
         /// </summary>
         public string AccountId { get; private set; }
-        
+
         /// <summary>
         /// A timestamp of the latest change of the user password, in milliseconds.
         /// </summary>
         public long? PasswordChangedTime { get; private set; }
-        
+
         /// <summary>
         /// A list of IDs of the groups this user belongs to.
         /// </summary>
@@ -48,60 +48,70 @@ namespace MbedCloudSDK.AccountManagement.Model.User
         /// Creation UTC time RFC3339.
         /// </summary>
         public DateTime? CreatedAt { get; private set; }
-        
+
         /// <summary>
         /// A flag indicating that the General Terms and Conditions has been accepted.
         /// </summary>
         public bool? TermsAccepted { get; set; }
-        
+
         /// <summary>
         /// The email address.
         /// </summary>
         public string Email { get; set; }
-        
+
         /// <summary>
         /// A flag indicating that receiving marketing information has been accepted.
         /// </summary>
         public bool? MarketingAccepted { get; set; }
-        
+
         /// <summary>
         /// The full name of the user.
         /// </summary>
         public string FullName { get; set; }
-        
+
         /// <summary>
         /// Address.
         /// </summary>
         public string Address { get; set; }
-        
+
         /// <summary>
         /// A timestamp of the user creation in the storage, in milliseconds.
         /// </summary>
         public long? CreationTime { get; private set; }
-        
+
         /// <summary>
         /// The password when creating a new user. It will will generated when not present in the request.
         /// </summary>
         public string Password { get; set; }
-        
+
         /// <summary>
         /// Phone number.
         /// </summary>
         public string PhoneNumber { get; set; }
-        
+
         /// <summary>
         /// The UUID of the user.
         /// </summary>
         [NameOverride(Name = "UserId")]
         [JsonProperty]
         public string Id { get; private set; }
-        
+
         /// <summary>
         /// A timestamp of the latest login of the user, in milliseconds.
         /// </summary>
         public long? LastLoginTime { get; private set; }
 
         /// <summary>
+        /// Whether two factor authentication has been enabled for this user.
+        /// </summary>
+        public bool? TwoFactorAuthentication { get; private set; }
+
+        /// <summary>
+        /// History of logins for this user.
+        /// </summary>
+        public List<LoginHistory> LoginHistory { get; private set; }
+
+        /// <summary>x
         /// Initializes new instance of User class.
         /// </summary>
         /// <param name="options">Dictionary containing properties.</param>
@@ -128,23 +138,25 @@ namespace MbedCloudSDK.AccountManagement.Model.User
         {
             var sb = new StringBuilder();
             sb.Append("class User {\n");
-            sb.Append("  Status: ").Append(Status).Append("\n");
-            sb.Append("  Username: ").Append(Username).Append("\n");
-            sb.Append("  EmailVerified: ").Append(EmailVerified).Append("\n");
-            sb.Append("  AccountId: ").Append(AccountId).Append("\n");
-            sb.Append("  PasswordChangedTime: ").Append(PasswordChangedTime).Append("\n");
-            sb.Append("  Groups: ").Append(Groups).Append("\n");
-            sb.Append("  CreatedAt: ").Append(CreatedAt).Append("\n");
-            sb.Append("  TermsAccepted: ").Append(TermsAccepted).Append("\n");
-            sb.Append("  Email: ").Append(Email).Append("\n");
-            sb.Append("  MarketingAccepted: ").Append(MarketingAccepted).Append("\n");
             sb.Append("  FullName: ").Append(FullName).Append("\n");
-            sb.Append("  Address: ").Append(Address).Append("\n");
-            sb.Append("  CreationTime: ").Append(CreationTime).Append("\n");
+            sb.Append("  Username: ").Append(Username).Append("\n");
             sb.Append("  Password: ").Append(Password).Append("\n");
+            sb.Append("  Email: ").Append(Email).Append("\n");
             sb.Append("  PhoneNumber: ").Append(PhoneNumber).Append("\n");
+            sb.Append("  Address: ").Append(Address).Append("\n");
+            sb.Append("  TermsAccepted: ").Append(TermsAccepted).Append("\n");
+            sb.Append("  MarketingAccepted: ").Append(MarketingAccepted).Append("\n");
+            sb.Append("  Groups: ").Append(Groups).Append("\n");
             sb.Append("  Id: ").Append(Id).Append("\n");
+            sb.Append("  Status: ").Append(Status).Append("\n");
+            sb.Append("  AccountId: ").Append(AccountId).Append("\n");
+            sb.Append("  EmailVerified: ").Append(EmailVerified).Append("\n");
+            sb.Append("  CreatedAt: ").Append(CreatedAt).Append("\n");
+            sb.Append("  CreationTime: ").Append(CreationTime).Append("\n");
+            sb.Append("  PasswordChangedTime: ").Append(PasswordChangedTime).Append("\n");
+            sb.Append("  TwoFactorAuthentication: ").Append(TwoFactorAuthentication).Append("\n");            
             sb.Append("  LastLoginTime: ").Append(LastLoginTime).Append("\n");
+            sb.Append("  loginHistory: ").Append(LoginHistory).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
         }
@@ -175,6 +187,8 @@ namespace MbedCloudSDK.AccountManagement.Model.User
             user.PhoneNumber = userInfo.PhoneNumber;
             user.Id = userInfo.Id;
             user.LastLoginTime = userInfo.LastLoginTime;
+            user.TwoFactorAuthentication = userInfo.IsTotpEnabled;
+            user.LoginHistory = Model.User.LoginHistory.MapList(userInfo.LoginHistory);
             return user; 
         }
 
