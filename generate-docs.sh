b5f7f826631356edea4868eb54c0d5b50df43170
alexl0gan <alex.logan@arm.com>
2017-09-20 11:05:35 +0100

    artifacts

--- generate-docs.sh
+++ generate-docs.sh
@@ -12,5 +12,5 @@ if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
   export AWS_ACCESS_KEY_ID=$AWS_ID;
   export AWS_SECRET_ACCESS_KEY=$AWS_SECRET;
   export AWS_DEFAULT_REGION=us-west-2;
-  aws s3 sync --delete --cache-control max-age=3600 docs/ s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
+  aws s3 sync --delete --cache-control max-age=3600 $OUTPUT_DIR s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
 fi
