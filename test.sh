bd0363919892204835b80e144f67c449ed9e8896
alexl0gan <alex.logan@arm.com>
2017-09-21 13:42:29 +0100

    pipe test output to ansi

--- test.sh
+++ test.sh
@@ -29,8 +29,7 @@ git clone https://$GITHUB_TOKEN@github.com/ARMmbed/mbed-cloud-sdk-testrunner.git
 pip install -r TestServer/testrunner/requirements.txt
 
 # Start the Python SDK test backend server. Send to background.
-CMD="mono TestServer/bin/Release/TestServer.exe $API_KEY"
-eval "$CMD &"
+mono TestServer/bin/Release/TestServer.exe $API_KEY & | ansi-html >> ${SDK_TESTS}/index.html
 echo "Backend server started. PID: $!"
 BACKEND_PID=$!
 
