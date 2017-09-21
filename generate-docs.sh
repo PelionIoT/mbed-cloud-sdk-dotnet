6639a8a61905752d859a2b02d3aab73f644c85e9
alexl0gan <alex.logan@arm.com>
2017-09-21 09:34:25 +0100

    revert circle changes

--- generate-docs.sh
+++ generate-docs.sh
@@ -16,4 +16,6 @@ if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
   export AWS_DEFAULT_REGION=us-west-2;
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/build
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TPIP} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/tpip
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TEST} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/test
 fi
--- test.sh
+++ test.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
-BACKEND_URL = "http://localhost:3000";
+BACKEND_URL="http://localhost:3000";
 export PYTHONPATH="TestServer/testrunner";
+
+cleanup() {
+  echo "Test run finished. Cleaning up. Deleting tmp directory: $TMPDIR";
+  if is_running $BACKEND_PID; then
+    echo "Killing backend SDK server: $BACKEND_PID";
+    kill $BACKEND_PID;
+  fi
+}
+
+is_running() {
+  if ps -p $1 >/dev/null; then
+    return 0;
+  fi
+  return 1;
+}
+
+# Ensure we have API key
+API_KEY="${MBED_CLOUD_API_KEY}"
+if [ -z $API_KEY ]; then
+  >&2 echo "API Key needs to be set using MBED_CLOUD_API_KEY env var";
+  exit 1;
+fi
+
+git clone https://$GITHUB_TOKEN@github.com/ARMmbed/mbed-cloud-sdk-testrunner.git "TestServer/testrunner"
+pip install -r TestServer/testrunner/requirements.txt
 
 # Start the Python SDK test backend server. Send to background.
 CMD="mono TestServer/bin/Release/TestServer.exe $API_KEY"
@@ -9,8 +34,18 @@ eval "$CMD &"
 echo "Backend server started. PID: $!"
 BACKEND_PID=$!
 
+sleep 2
+
+if ! is_running $BACKEND_PID; then
+  >&2 echo "Backend server did not start successfully."
+  cleanup
+  exit 1000
+fi
+
 # Start the test runner
 python TestServer/testrunner/bin/trunner -s $BACKEND_URL -k $API_KEY
 RET_CODE=$?
+
+cleanup
 
 exit $RET_CODE
