6d6a52dadc596c9bad40b2393fb49b7598b7495c
alexl0gan <alex.logan@arm.com>
2017-09-27 13:02:40 +0100

    move doc generation

--- generate-docs.sh
+++ generate-docs.sh
@@ -8,13 +8,3 @@ OUTPUT_DIR="docs/";
 doxygen ar.doxygen
 
 mv docs/* ${SDK_DOCS}
-
-# If AWS_ID and SECRET is defined, we push to S3
-if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
-  export AWS_ACCESS_KEY_ID=$AWS_ID;
-  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET;
-  export AWS_DEFAULT_REGION=us-west-2;
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/build
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/tests
-fi
