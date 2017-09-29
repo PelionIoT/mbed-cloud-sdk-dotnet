de331ca709a91868cdf31cf4087cdf48a157fd01
Alex Logan <30593755+alexl0gan@users.noreply.github.com>
2017-09-29 11:35:30 +0100

    Upload master to latest folder in s3 (#103)
    * only upload master to latest
    
    * permish
    
    * swap scripts
    
    * Update README.md

--- scripts/uploadArtifactsMaster.sh
+++ scripts/uploadArtifactsMaster.sh
@@ -8,4 +8,8 @@ if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/build
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/tests
+  echo "Uploading to latest"
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/latest/docs
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/latest/build
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/latest/tests
 fi
