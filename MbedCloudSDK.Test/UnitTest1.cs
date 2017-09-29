eb4f4b9d82c9ef5f0de32abb9c383a89dce601c7
alexl0gan <alex.logan@arm.com>
2017-09-29 07:30:15 -0700

    add nunit

---
+++ MbedCloudSDK.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MbedCloudSDK.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("test", "test");
+        }
+    }
+}
