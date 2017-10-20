9f953c620a6711d73ed37605b4ee81ac4048a11f
Alex Logan <Alex.Logan@arm.com>
2017-10-20 10:18:20 +0100

    change folder structure

--- scripts/uploadArtifacts.sh
+++ scripts/uploadArtifacts.sh
@@ -6,6 +6,5 @@ if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
   export AWS_DEFAULT_REGION=us-west-2;
   echo "Uploading to ${SDK_TAG}"
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/build
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/tests
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_REPORTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/reports
 fi
--- scripts/uploadArtifactsMaster.sh
+++ scripts/uploadArtifactsMaster.sh
@@ -6,10 +6,8 @@ if [[ -n $AWS_ID && -n $AWS_SECRET ]]; then
   export AWS_DEFAULT_REGION=us-west-2;
   echo "Uploading to ${SDK_TAG}"
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/build
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/tests
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_REPORTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/reports
   echo "Uploading to latest"
   aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/latest/docs
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_BUILD} s3://mbed-cloud-sdk-dotnet-dist/latest/build
-  aws s3 sync --delete --cache-control max-age=3600 ${SDK_TESTS} s3://mbed-cloud-sdk-dotnet-dist/latest/tests
+  aws s3 sync --delete --cache-control max-age=3600 ${SDK_REPORTS} s3://mbed-cloud-sdk-dotnet-dist/latest/reports
 fi
