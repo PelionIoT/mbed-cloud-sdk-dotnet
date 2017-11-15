219b23f7a1638976ea05bbf6a53178b3334c3d79
Alex Logan <Alex.Logan@arm.com>
2017-11-15 18:38:59 +0000

    restore example packages in circle

--- ConsoleExamples/Properties/AssemblyInfo.cs
+++ ConsoleExamples/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleExamples")]
+[assembly: AssemblyTitle(nameof(ConsoleExamples))]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arm")]
-[assembly: AssemblyProduct("ConsoleExamples")]
+[assembly: AssemblyProduct(nameof(ConsoleExamples))]
 [assembly: AssemblyCopyright("Copyright © Arm 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
