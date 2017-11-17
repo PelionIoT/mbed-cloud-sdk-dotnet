dfc46a23822553d904aa92b0585abb19e5ae5934
Alex Logan <Alex.Logan@arm.com>
2017-11-17 17:17:33 +0000

    fix date format in update campaign

--- MbedCloudSDK/Update/Api/UpdateApi.cs
+++ MbedCloudSDK/Update/Api/UpdateApi.cs
@@ -4,6 +4,7 @@
 
 namespace MbedCloudSDK.Update.Api
 {
+    using System;
     using System.Linq;
     using MbedCloudSDK.Common;
     using update_service.Client;
@@ -13,6 +14,7 @@ namespace MbedCloudSDK.Update.Api
     /// </summary>
     public partial class UpdateApi : BaseApi
     {
+        private Config config;
         private update_service.Api.DefaultApi api;
 
         /// <summary>
@@ -27,10 +29,11 @@ namespace MbedCloudSDK.Update.Api
                 Configuration.Default.ApiClient = new ApiClient(config.Host);
             }
 
+            this.config = config;
             api = new update_service.Api.DefaultApi();
             api.Configuration.ApiKey["Authorization"] = config.ApiKey;
             api.Configuration.ApiKeyPrefix["Authorization"] = config.AuthorizationPrefix;
-            api.Configuration.DateTimeFormat = "YYYY-MM-DDThh:mm:ss.uuuuuuZ";
+            api.Configuration.DateTimeFormat = "yyyy-MM-ddThh:mm:ss.ffffffZ";
         }
 
         /// <summary>
@@ -40,6 +43,69 @@ namespace MbedCloudSDK.Update.Api
         public static ApiMetadata GetLastApiMetadata()
         {
             return ApiMetadata.Map(update_service.Client.Configuration.Default.ApiClient.LastApiResponse.LastOrDefault());
+        }
+
+        /// <summary>
+        /// TestTest
+        /// </summary>
+        /// <returns>Campaign</returns>
+        public Model.Campaign.Campaign TestTest()
+        {
+            var manifestId = string.Empty;
+
+            // List all firware manifests
+            var listParamas = new Common.Query.QueryOptions
+            {
+                Limit = 5,
+            };
+
+            var manifests = ListFirmwareManifests(listParamas).Data;
+            foreach (var item in manifests)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            manifestId = manifests.LastOrDefault()?.Id;
+
+            // List all queries
+            var devicesApi = new DeviceDirectory.Api.DeviceDirectoryApi(config);
+            var queries = devicesApi.ListQueries();
+            var query = queries.LastOrDefault();
+
+            // Create Campaign
+            var campaignName = System.DateTime.Now.ToString();
+            var campaign = new Model.Campaign.Campaign
+            {
+                Name = campaignName,
+                RootManifestId = manifestId,
+                DeviceFilter = query.Filter,
+            };
+
+            campaign = AddCampaign(campaign);
+
+            Console.WriteLine("Created campaign : " + campaign);
+
+            // Start update campaign
+            campaign = StartCampaign(campaign);
+
+            // Print status of update campaign
+            var countdown = 10;
+            while (countdown >= 0)
+            {
+                var options = new Common.Query.QueryOptions
+                {
+                    Id = campaign.Id,
+                };
+                var states = ListCampaignDeviceStates(options).Data;
+                foreach (var item in states)
+                {
+                    Console.WriteLine($"{item.Id} - {Convert.ToString(item.State)}");
+                }
+
+                countdown--;
+            }
+
+            return campaign;
         }
     }
 }
