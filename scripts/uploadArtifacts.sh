d2e384e644048ef2a14adcea6bf78e25e56fcaa9
Alex Logan <Alex.Logan@arm.com>
2017-12-19 14:01:49 +0000

    temp branch change

--- scripts/uploadArtifacts.sh
+++ scripts/uploadArtifacts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export AWS_DEFAULT_REGION=us-west-2;
 echo "Uploading to ${SDK_TAG}"
 aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
 aws s3 sync --delete --cache-control max-age=3600 ${SDK_REPORTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/reports
