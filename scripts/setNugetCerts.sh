8bdd160a1e7c1bcffc0ecea4a44facfd7e995618
Alex Logan <Alex.Logan@arm.com>
2018-01-23 16:22:34 +0000

    destroy!

--- scripts/setNugetCerts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-yes "yes" | sudo certmgr -ssl -m https://go.microsoft.com
-yes "yes" | sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
-yes "yes" | sudo certmgr -ssl -m https://nuget.org
--- scripts/uploadArtifacts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Uploading to ${SDK_TAG}"
-aws s3 sync --delete --cache-control max-age=3600 ${SDK_DOCS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/docs
-aws s3 sync --delete --cache-control max-age=3600 ${SDK_REPORTS} s3://mbed-cloud-sdk-dotnet-dist/${SDK_TAG}/reports
