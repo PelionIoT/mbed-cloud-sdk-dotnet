0c53b0ca9ab29f3811c9462132eb364cfb623e35
Alex Logan <Alex.Logan@arm.com>
2018-01-23 16:52:33 +0000

    add dotnet script

---
+++ scripts/dotnet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+INSTALLDIR="cli-tools"
+CLI_VERSION=1.0.1
+DOWNLOADER=$(which curl)
+if [ -d "$INSTALLDIR" ]
+then
+    rm -rf "$INSTALLDIR"
+fi
+mkdir -p "$INSTALLDIR"
+echo Downloading the CLI installer.
+$DOWNLOADER https://dot.net/v1/dotnet-install.sh > "$INSTALLDIR/dotnet-install.sh"
+chmod +x "$INSTALLDIR/dotnet-install.sh"
+echo Installing the CLI requested version $CLI_VERSION. Please wait, installation may take a few minutes.
+"$INSTALLDIR/dotnet-install.sh" --install-dir "$INSTALLDIR" --version $CLI_VERSION
+if [ $? -ne 0 ]
+then
+    echo Download of $CLI_VERSION version of the CLI failed. Exiting now.
+    exit 0
+fi
+echo The CLI has been installed.
+LOCALDOTNET="$INSTALLDIR/dotnet"
+# Run the build process now. Implement your build script here.
