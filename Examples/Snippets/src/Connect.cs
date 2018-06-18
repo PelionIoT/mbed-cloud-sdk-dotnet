a1e0aa33d1ca5f261f21df8c09ccdda6ae5b7d29
Alex Logan <Alex.Logan@arm.com>
2018-06-18 16:34:18 +0100

    Add snippet project

---
+++ Examples/Snippets/src/Connect.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using MbedCloudSDK.Common;
+using MbedCloudSDK.Connect.Api;
+using MbedCloudSDK.Connect.Api.Subscribe.Models;
+
+namespace Snippets.src
+{
+    public class Connect
+    {
+        public void SubscribeToDeviceStateChanges()
+        {
+            // example: subscribing to device state changes
+            var config = new Config("An MbedCloud Api  Key", "custom host url");
+
+            using (var connect = new ConnectApi(config))
+            {
+
+                connect.Subscribe.DeviceEvents().Where(d => d.Event == DeviceEventEnum.Registration).OnNotify += (res) => Console.WriteLine(res);
+
+                Thread.Sleep(120000);
+            }
+            // end of example
+        }
+
+        public void SubscribeToResourceVslueChanges()
+        {
+            // example: subscribing to resource value changes
+            var config = new Config("An MbedCloud Api  Key", "custom host url");
+
+            using (var connect = new ConnectApi(config))
+            {
+
+                connect.Subscribe.ResourceValues("016*", "/3/0/*").OnNotify += (res) => Console.WriteLine(res);
+
+                Thread.Sleep(120000);
+            }
+            // end of example
+        }
+    }
+}
