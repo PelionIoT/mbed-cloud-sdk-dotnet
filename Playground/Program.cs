5b9c65a8e901dd32de7bda2988a61a0258180a99
Alex Logan <30593755+alexl0gan@users.noreply.github.com>
2018-07-09 17:26:14 +0100

    ⭕️ circle improvements (#194)
    * use version for tag
    
    * 2.1?
    
    * tc remove
    
    * doh
    
    * more shaving
    
    * no shave
    
    * codecov
    
    * restore news
    
    * new towncrier

---
+++ Playground/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MbedCloudSDK.Billing.Api;
+using MbedCloudSDK.Common;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("MBED_CLOUD_API_KEY");
+            var host = Environment.GetEnvironmentVariable("MBED_CLOUD_API_HOST");
+
+            var config = new Config(apiKey, host);
+
+            var billing = new BillingApi(config);
+
+            
+        }
+    }
+}
