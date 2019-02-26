70358a4972372311b43899047d8cfef326306669
Alex Logan <Alex.Logan@arm.com>
2019-02-26 14:45:28 +0000

    more delete

--- MbedCloudSDK/Connect/Api/Subscribe/Collections/FilterFunctionCollection.cs
+++
@@ -1,9 +0,0 @@
-namespace MbedCloudSDK.Connect.Api.Subscribe.Collections
-{
-    using System;
-    using System.Collections.Generic;
-
-    public class FilterFunctionCollection<T> : List<Func<T, bool>>
-    {
-    }
-}
