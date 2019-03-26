26efdd4c0e1466281aa2ee401518e978c1f95aff
Alex Logan <Alex.Logan@arm.com>
2019-03-26 10:35:09 +0000

    certificate blacklist example

---
+++ Examples/Snippets/src/Foundation/CertificateBlacklist.cs
@@ -0,0 +1,51 @@
+using System;
+using Mbed.Cloud;
+using Mbed.Cloud.Foundation;
+using Mbed.Cloud.Foundation.Enums;
+using MbedCloudSDK.Exceptions;
+using NUnit.Framework;
+
+[TestFixture]
+public class CertificateBlacklist
+{
+    [Test]
+    public async System.Threading.Tasks.Task CertificateBlacklistExampleAsync()
+    {
+        try
+        {
+            var certId = "016711e8061a5ad1ecb35f6800000000";
+            // an example: certificate_black_listing
+            var sdk = new SDK();
+
+            var trustedCert = await sdk
+                                    .Foundation()
+                                    .TrustedCertificateRepository()
+                                    .Read(certId);
+
+            trustedCert.Status = TrustedCertificateStatus.INACTIVE;
+
+            await sdk
+                    .Foundation()
+                    .TrustedCertificateRepository()
+                    .Update(trustedCert.Id, trustedCert);
+
+            var deviceList = sdk
+                                .Foundation()
+                                .DeviceEnrollmentDenialRepository()
+                                .List(
+                                    new DeviceEnrollmentDenialListOptions()
+                                        .TrustedCertificateIdEqualTo(trustedCert.Id));
+
+            foreach (var device in deviceList)
+            {
+                Console.WriteLine($"Device endpoint name: {device.EndpointName}");
+            }
+            // end of example
+        }
+        catch (CloudApiException e) when (e.ErrorCode == 400)
+        {
+            // currently expecting 400 as not activated in account
+        }
+    }
+
+}
