1689abc7c9427a13770df544f925fa6f65855126
Alex Logan <Alex.Logan@arm.com>
2019-05-17 16:57:04 +0100

    add legacy examples

--- Examples/Snippets/src/Legacy/Resources.cs
+++ Examples/Snippets/src/Legacy/Resources.cs
@@ -11,8 +11,39 @@ namespace Snippets.src.Legacy
         [Test]
         public void ReadingAResource()
         {
+            // an example: legacy_get_resource
+            // cloak
+            /*
+            // uncloak
+            using Mbed.Cloud.Common;
+            using MbedCloudSDK.Billing.Api;
+            // cloak
+            */
+            // uncloak
             var billingApi = new BillingApi(new Config());
             Console.WriteLine($"Quota remaining: {billingApi.GetQuotaRemaining()}");
+            // end of example
+
+        }
+
+        [Test]
+        public void ListingAResource()
+        {
+            // an example: legacy_listing_resources
+            // cloak
+            /*
+            // uncloak
+            using Mbed.Cloud.Common;
+            using MbedCloudSDK.Billing.Api;
+            // cloak
+            */
+            // uncloak
+            var billingApi = new BillingApi(new Config());
+            foreach (var quotaHistory in billingApi.GetQuotaHistory())
+            {
+                Console.WriteLine($"Quota change reason: {quotaHistory.Reason}, delta: {quotaHistory.Delta}");
+            }
+            // end of example
         }
     }
 }
