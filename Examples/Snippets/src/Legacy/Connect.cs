c5f7e280e428a59a92e334570661009b57194aa1
Alex Logan <Alex.Logan@arm.com>
2019-07-17 16:08:17 +0100

    update connect examples

--- Examples/Snippets/src/Legacy/Connect.cs
+++ Examples/Snippets/src/Legacy/Connect.cs
@@ -2,46 +2,38 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Mbed.Cloud;
 using Mbed.Cloud.Common;
+using Mbed.Cloud.Foundation;
+using Mbed.Cloud.Foundation.Enums;
 using MbedCloudSDK.Connect.Api;
-using MbedCloudSDK.Connect.Api.Subscribe.Models;
 
 namespace Snippets.src
 {
     public class Connect
     {
-        public async Task SubscribeToDeviceStateChanges()
-        {
-            // an example: subscribing to device state changes
-            var config = new Config();
-
-            using (var connect = new ConnectApi(config))
-            {
-                var observer = (await connect.Subscribe.DeviceEventsAsync()).Filter(d => d.Event == DeviceEvent.Registration);
-
-                observer.OnNotify += (res) => Console.WriteLine(res);
-
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(await observer.NextAsync());
-                }
-            }
-            // end of example
-        }
-
         public async Task SubscribeToResourceVslueChanges()
         {
-            // an example: subscribing to resource value changes
-            var config = new Config();
+            // an example: subscribe to resource values
+            // cloak
+            /*
+            // uncloak
+            using Mbed.Cloud.Common;
+            using MbedCloudSDK.Connect.Api;
+            // cloak
+            */
+            // uncloak
+
+            var config = new Config
+            {
+                AutostartNotifications = true,
+            };
 
             using (var connect = new ConnectApi(config))
             {
-                var observer = await connect.Subscribe.ResourceValuesAsync("016*", "/3/0/*");
+                var observer = await connect.Subscribe.ResourceValuesAsync("*");
 
-                observer.OnNotify += (res) => Console.WriteLine(res);
-
-                for (int i = 0; i < 10; i++)
-                {
+                while(true) {
                     Console.WriteLine(await observer.NextAsync());
                 }
             }
@@ -50,7 +42,28 @@ namespace Snippets.src
 
         public void GetAndSetResourceValues()
         {
-            // an example: resource values
+            var sdk = new SDK();
+            // an example: get and set a resource value
+            // cloak
+            /*
+            // uncloak
+            using Mbed.Cloud;
+            using Mbed.Cloud.Common;
+            using Mbed.Cloud.Foundation;
+            using Mbed.Cloud.Foundation.Enums;
+            using MbedCloudSDK.Connect.Api;
+            // cloak
+            */
+            // uncloak
+
+            // Use the Foundation interface to find a connected device.
+            var device = sdk
+                            .Foundation()
+                            .DeviceRepository()
+                            .List(new DeviceListOptions().StateEqualTo(DeviceState.REGISTERED))
+                            .FirstOrDefault();
+
+            // Use the Legacy interface for find resources
             var config = new Config
             {
                 AutostartNotifications = true,
@@ -58,14 +71,18 @@ namespace Snippets.src
 
             using (var connect = new ConnectApi(config))
             {
-                var resource = connect.ListConnectedDevices()
-                                    .FirstOrDefault()
-                                    .ListResources()
+                // Find an observable resource
+                var resource = connect
+                                    .ListResources(device.Id)
                                     .FirstOrDefault(r => r.Observable == true);
 
-                connect.SetResourceValue(resource.DeviceId, resource.Path, DateTime.Now.ToString());
+                // Set a resource value
+                connect.SetResourceValue(resource.DeviceId, resource.Path, "12");
 
-                var newValue = connect.GetResourceValue(resource.DeviceId, resource.Path);
+                // Get a resource value
+                var value = connect.GetResourceValue(resource.DeviceId, resource.Path);
+
+                Console.WriteLine($"Device {device.Id}, path {resource.Path}, current value: {value}");
             }
             // end of example
         }
